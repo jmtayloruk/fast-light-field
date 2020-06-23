f0a7ab9986ce2606f780c6089c7b5459f568cecc
Jonathan Taylor <jonathan.taylor@glasgow.ac.uk>
2020-06-23 15:46:58 +0100

    Minor compile tweaks for beag-shuil

--- light-field-integrands/setup.py
+++ light-field-integrands/setup.py
@@ -1,13 +1,14 @@
-# Cribbed from https://saoghal.net/articles/2020/faster-numerical-integration-in-scipy
+# Cribbed from https://saoghal.net/articles/2020/faster-numerical-integration-in-scipy
 
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-setup(
-    ext_modules=cythonize(Extension("light_field_integrands",
-                                    sources=["light_field_integrands.pyx"],
-                                    include_dirs=['./', '/Volumes/Development/scatter/gsl/'],
-                                    extra_link_args=['-L../../scatter/gsl/.libs', '-lgsl']),
-                          annotate=True)
-)
+# Note the hard-coded relative path to scatter/gsl.
+# TODO: obviously that needs sorting out before this is releasable code
+# (and may need some contrived setting up on other platforms to make it work!)
+setup(ext_modules=cythonize(Extension("light_field_integrands",
+                                      sources=["light_field_integrands.pyx"],
+                                      include_dirs=['./', '../../scatter/gsl/'],
+                                      extra_link_args=['../../scatter/gsl/.libs/libgsl.a']),
+                            annotate=True))
