4b9e72f8d205730ac58627bddc3fe6df22f45a76
Jonny Taylor <jonathan.taylor@glasgow.ac.uk>
2022-02-19 19:52:22 +0000

    Remove transpose that I was doing when we load a camera image. Jose pointed out that our output volumes are transposed relative to what they should be. See comment in lfimage.py for more of the rationale behind this

--- lfimage.py
+++ lfimage.py
@@ -5,12 +5,16 @@ def LoadLightFieldTiff(imagePath, bigger=(1,1)):
     # Load the tiff file from disk
     image = tifffile.imread(imagePath)
     assert(len(image.shape) == 2)
+    
     # For compatibility with Prevedel's matlab code, I transpose the image we have loaded.
-    # This allows me to index it in the same order Prevedel does (given that Matlab indexes arrays in the opposite order to Python)
-    # TODO: that said, this is a bit of a confusing and unhelpful way to deal with that issue.
-    # I should probably handle it better somehow - but for now I am leaving all the code as-is since it works!
-    # The copy is to ensure contiguous strides in the final dimension, which my C code expects and requires
-    image = image.transpose().copy()
+    # For convenience, throughout my code I index x and y in the same order Prevedel does.
+    # Given that Matlab indexes arrays in the opposite order to Python, that would seem on the
+    # face of it to cause problems, that would require us to transpose the image when we load it here
+    # (and transpose it back again after we have completed the reconstruction).
+    # However I am 99% certain that symmetries in the light field PSF mean that we can get away without
+    # doing any of that transposing - hence why I have disabled this next line of code.
+    #image = image.transpose().copy()
+    
     # Convert to float32 and return.
     # If caller specifies 'bigger', we tile the image up to a larger size.
     # This is purely just a convenient way of generating larger images for performance stress-testing of my code.
